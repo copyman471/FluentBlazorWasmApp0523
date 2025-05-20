2ec5a386bd7c2fab0d97757d1fa170444a9f2ba0
copyman471 <copyman471@outlook.com>
2025-05-21 00:14:38 +0900

    feat: Add Card

--- wwwroot/js/full.js
+++ wwwroot/js/full.js
@@ -2,9 +2,9 @@
     const element = document.getElementById(elementId);
     if (element.requestFullscreen) {
         element.requestFullscreen();
-    } else if (element.webkitRequestFullscreen) { /* Safari */
+    } else if (element.webkitRequestFullscreen) {
         element.webkitRequestFullscreen();
-    } else if (element.msRequestFullscreen) { /* IE11 */
+    } else if (element.msRequestFullscreen) {
         element.msRequestFullscreen();
     }
 }
